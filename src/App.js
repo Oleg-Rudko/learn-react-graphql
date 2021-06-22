06b6dec30fc76bf0038c1e4e05b84de5ea0230de
Oleg Rudko <oleg.rudko0906@gmail.com>
2021-06-22 10:25:28 +0300

    add few line of code :

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
 
 const App = () => {
+  const { data } = useQuery(gql`
+    query {
+      todo {
+        name
+      }
+    }
+  `);
+  console.log(data);
   return <div>Hello world!</div>;
 };
 
