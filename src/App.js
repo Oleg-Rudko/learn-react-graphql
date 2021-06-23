f181d085e2249fd60639ef000a27ef15ca43faee
Ihor Salfetnikov <salfetnikov92@gmail.com>
2021-06-23 10:10:13 +0300

    add react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from "react";
-import { gql, useQuery } from "@apollo/client";
 
 const App = () => {
-  const { data } = useQuery(gql`
-    query {
-      todo {
-        name
-      }
-    }
-  `);
-  console.log(data);
-  return <div>Hello world!</div>;
+  return <div className='App'>
+    Hello world
+  </div>;
 };
 
 export default App;
