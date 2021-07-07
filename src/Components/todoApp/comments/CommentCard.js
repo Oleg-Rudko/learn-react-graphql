56c7361d0c7f757e1fed4063189cb2dd25d01760
Ihor Salfetnikov <salfetnikov92@gmail.com>
2021-07-07 18:40:39 +0300

    Added the ability to delete a comment
    In the <CommentCard /> component, when the x button is pressed,
    the comment is deleted.

--- src/Components/todoApp/comments/CommentCard.js
+++ src/Components/todoApp/comments/CommentCard.js
@@ -1,13 +1,42 @@
 import React from "react";
+import { useMutation, gql } from "@apollo/client";
+import { useParams } from "react-router";
 
-const CommentCard = ({ dataComment }) => {
+const CommentCard = ({ dataComment, refetch }) => {
+  const {id} = useParams();
+  const [removeComment] = useMutation(
+    gql`
+      mutation RemoveComments($todo_id: uuid, $id: uuid!) {
+        delete_comments(
+          where: { todo_id: { _eq: $todo_id }, id: { _eq: $id } }
+        ) {
+          affected_rows
+        }
+      }
+    `
+  );
+
+  const removeCommentTodo = () => {
+    removeComment({
+      variables: {
+        id: dataComment.id,
+        todo_id: id,
+      },
+    }).then(() => refetch());
+  };
+
   return (
     <div className="commentCard">
       <div className="commentCard_description">
         <div className="commentCard_description-text">
           {dataComment.description}
         </div>
-        <button className="commentCard_description-btn">x</button>
+        <button
+          onClick={removeCommentTodo}
+          className="commentCard_description-btn"
+        >
+          x
+        </button>
       </div>
       <p className="commentCard_date-created">
         Was created {dataComment.date_created}
