46e2ccea1d2077a9c8c6d68cd1c4b9feac737d8f
Ihor Salfetnikov <salfetnikov92@gmail.com>
2021-07-13 12:43:24 +0300

    Change icons in todo app
    Installed react-bootstrap-icons library.
    Replacing an image SVG to icons in App.

--- src/Components/todoApp/buttonsControl/ButtonShowComments.js
+++ src/Components/todoApp/buttonsControl/ButtonShowComments.js
@@ -1,24 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import ShowIsComment from "../../../img/showIsComment.svg";
-import ShowNotComment from "../../../img/showNotComment.svg";
+import { ChatFill, ChatDots } from "react-bootstrap-icons";
 
 const ButtonComment = ({ todoId, countComment }) => {
   return (
     <button className="showComments" title="Open comments to this todo">
       <Link to={`/comments/${todoId}`}>
         {countComment ? (
-          <img
-            className="showComments_img"
-            src={ShowIsComment}
-            alt="Show comments"
-          />
+          <ChatDots color="#6bd15a" size="30" />
         ) : (
-          <img
-            className="showComments_img"
-            src={ShowNotComment}
-            alt="Add comments"
-          />
+          <ChatFill color="#6c757d" size="30" />
         )}
       </Link>
     </button>
