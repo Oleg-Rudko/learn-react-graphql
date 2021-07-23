389c0412ee995e41f0d3fc08a1b55fc72c0d864e
Ihor Salfetnikov <salfetnikov92@gmail.com>
2021-07-23 21:43:04 +0300

    Add icon to edit user credential
    Was added icon current user.
    The created user page in which all personal information is displayed.
    Added the ability to edit personal data and save it.

--- src/Components/Routing.js
+++ src/Components/Routing.js
@@ -4,6 +4,7 @@ import SignUp from "./authorization/signUp/SignUp";
 import Login from "./authorization/login/Login";
 import Auth from "./authorization/Auth";
 import Comments from "./todoApp/comments/Comments";
+import EditPersonalData from "./todoApp/EditPersonalData";
 
 const Routing = () => {
   return (
@@ -13,6 +14,7 @@ const Routing = () => {
         <Route path="/sign-up" component={SignUp} />
         <Route path="/login" component={Login} />
         <Route path="/comments/:id" component={Comments} />
+        <Route path="/edit-personal-data" component={EditPersonalData} />
       </Switch>
       <Redirect to="/" />
     </>
