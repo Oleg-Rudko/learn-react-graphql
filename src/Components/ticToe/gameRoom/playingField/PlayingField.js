a075b6a0a4198526fafe92459b90da05ef23568b
Ihor Salfetnikov <salfetnikov92@gmail.com>
2021-08-04 18:00:21 +0300

    Development of the playing field
    Created a 3x3 playing field.
    Locally, one player can click on the field and "X" or "0" will be put in turn.

--- src/Components/ticToe/gameRoom/playingField/PlayingField.js
+++ src/Components/ticToe/gameRoom/playingField/PlayingField.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 const PlayingField = () => {
-  return <div className="playingField">Playing Field</div>;
+  const [symbol, setSymbol] = useState("X");
+  const [arrField, setArrField] = useState([
+    { id: 1, symbol: "" },
+    { id: 2, symbol: "" },
+    { id: 3, symbol: "" },
+    { id: 4, symbol: "" },
+    { id: 5, symbol: "" },
+    { id: 6, symbol: "" },
+    { id: 7, symbol: "" },
+    { id: 8, symbol: "" },
+    { id: 9, symbol: "" },
+  ]);
+
+  const makeMove = (index) => {
+    if (symbol === "0") {
+      setSymbol("X");
+    } else if (symbol === "X") {
+      setSymbol("0");
+    }
+
+    setArrField([
+      ...arrField.map((item) =>
+        item.id === index && item.symbol === ""
+          ? { ...item, symbol: symbol }
+          : item
+      ),
+    ]);
+  };
+
+  return (
+    <div className="playingField">
+      <div className="playingField_wrap">
+        {arrField.map((item) => (
+          <div
+            key={item.id}
+            onClick={() => makeMove(item.id)}
+            className="field_row"
+          >
+            {item.symbol}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default PlayingField;
